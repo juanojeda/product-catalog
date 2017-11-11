157727b8655f2d1493ab334ab7f6176b3135183a
Juan Ojeda <juan@juanojeda.com>
2017-11-11 13:15:35 +1100

    ## Not quite a boilerplate... - add webpack and gulp for modern dev tooling - add react as MVCish framework

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const root = document.getElementById('root');
+
+if (root) {
+  ReactDOM.render(
+    <div>Test</div>,
+    root
+  );
+}
